52267a95ffb08f62a82f1fe39069e5909e9da9fd
Anca Simon <ancarsimon@gmail.com>
2020-05-04 10:14:32 -0500

    built out destinations section

---
+++ src/javascripts/components/destinationsContainer/destinationsContainer.js
@@ -0,0 +1,23 @@
+import destinationCard from '../destinationCard/destinationCard';
+
+import destinationData from '../../helpers/data/destinationData';
+
+import utils from '../../helpers/utils';
+
+const buildDestinationsContainer = () => {
+  destinationData.getDestinations()
+    .then((destinations) => {
+      let domString = '';
+      domString += '<div class="d-flex flex-wrap">';
+      destinations.forEach((item) => {
+        domString += '<div class="col-md-4">';
+        domString += destinationCard.buildDestinationCard(item);
+        domString += '</div>';
+      });
+      domString += '</div>';
+      utils.printToDom('destinationsDiv', domString);
+    })
+    .catch((error) => console.error('get destinations broke', error));
+};
+
+export default { buildDestinationsContainer };
--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -5,6 +5,7 @@ const loginDiv = $('#login-page');
 const homeDiv = $('#home-page');
 const homeButton = $('#navbar-home-button');
 const logoutButton = $('#navbar-logout-button');
+const destinationsDiv = $('#destinationsDiv');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
@@ -14,12 +15,14 @@ const checkLoginStatus = () => {
       homeDiv.removeClass('hide');
       homeButton.removeClass('hide');
       logoutButton.removeClass('hide');
+      destinationsDiv.removeClass('hide');
     } else {
       // person is not logged in
       loginDiv.removeClass('hide');
       homeDiv.addClass('hide');
       homeButton.addClass('hide');
       logoutButton.addClass('hide');
+      destinationsDiv.addClass('hide');
     }
   });
 };
